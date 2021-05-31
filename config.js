be42b2fe6de7f00690caf1a60dcfa8c4b8112a2b
cjmcclure1337 <62575120+cjmcclure1337@users.noreply.github.com>
2021-05-31 10:38:16 -0500

    OAuth Facebook Passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost: 27017/nucampsite"
+    "mongoUrl": "mongodb://localhost: 27017/nucampsite",
+    "facebook": {
+        clientId: "172243561509456",
+        clientSecret: "4618f470dd170c3fe26c11dfd2117bb9"
+    }
 }
